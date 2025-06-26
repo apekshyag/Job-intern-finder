680fadf5d53bb51cdacabb8b11a3c52a54205d68
apekshyag <apekshyaghimire41@gmail.com>
2025-06-26 09:47:15 +0545

    react router fixation

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import Navbar from './components/NavBar'
+import { BrowserRouter as Router } from 'react-router-dom'
 
 const App = () => {
   return (
-   <>
-   <Navbar/>
-   </>
+   <Router>
+     <Navbar/>
+
+   </Router>
+   
+
+  
+   
+  
   )
 }
 
